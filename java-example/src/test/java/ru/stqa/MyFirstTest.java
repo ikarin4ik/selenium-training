9ab053df2305593266b862a5cd99a443ed9d1157
Karina Garmash <karina@garmash.org>
2017-09-20 22:49:03 +0500

    first test

---
+++ java-example/src/test/java/ru/stqa/MyFirstTest.java
@@ -0,0 +1,39 @@
+package ru.stqa;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start () {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait (driver, 10);
+    }
+
+    @Test
+    public void myFirstTest () {
+        driver.get("https://www.google.ru/");
+        driver.findElement(By.name("q")).sendKeys("webdriver", Keys.ENTER);
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+        }
+    }
+
+
