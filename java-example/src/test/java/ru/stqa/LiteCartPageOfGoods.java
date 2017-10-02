29b2504b71669426457ce19c3279c985eaef8032
Karina Garmash <karina@garmash.org>
2017-10-02 21:59:03 +0500

    Задание 9 Проверить сортировку стран и геозон в админке

---
+++ java-example/src/test/java/ru/stqa/LiteCartPageOfGoods.java
@@ -0,0 +1,30 @@
+package ru.stqa;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LiteCartPageOfGoods {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+}
--- java-example/src/test/java/ru/stqa/SortCountriesTest.java
+++ java-example/src/test/java/ru/stqa/SortCountriesTest.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,19 +16,21 @@ import java.util.List;
 
 public class SortCountriesTest {
 
-    public static final String COUNTRIES_URL = "?app=countries&doc=countries";
     private static final String BASE_URL = "http://localhost/litecart/admin";
+    private static final String COUNTRIES_URL = "?app=countries&doc=countries";
+    private static final String ZONES_URL = "?app=geo_zones&doc=geo_zones";
+    private static final String COUNTRIES_SELECTOR = "td#content form[name=countries_form] table.dataTable tbody tr.row";
+    private static final String ZONES_SELECTOR = "td#content form table.dataTable tbody tr.row";
+
 
     private WebDriver driver;
-    private WebDriverWait wait;
 
     @Before
     public void start() {
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
     }
 
-    public void login() {
+    private void login() {
         driver.get(BASE_URL);
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
@@ -38,15 +39,15 @@ public class SortCountriesTest {
     }
 
 
-    public void logout () {
+    private void logout() {
         driver.findElement(By.cssSelector("td#sidebar div.header a[title='Logout'] ")).click();
     }
 
     @Test()
-    public void verifyCountries () {
+    public void verifyCountries() {
         login();
         driver.get(BASE_URL + COUNTRIES_URL);
-        ArrayList <String> list = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<>();
         List<WebElement> elements = driver.findElements(
                 By.cssSelector("td#content form[name=countries_form] table.dataTable tbody tr.row :nth-child(5)"));
 
@@ -65,13 +66,13 @@ public class SortCountriesTest {
     }
 
     @Test()
-    public void verifyCountriesZones () {
+    public void verifyCountriesZones() {
         login();
         driver.get(BASE_URL + COUNTRIES_URL);
-        List<WebElement> elements = driver.findElements(By.cssSelector("td#content form[name=countries_form] table.dataTable tbody tr.row"));
+        List<WebElement> elements = driver.findElements(By.cssSelector(COUNTRIES_SELECTOR));
         for (int i = 0; i < elements.size(); i++) {
             WebElement zoneCount = elements.get(i).findElement(By.cssSelector(":nth-child(6)"));
-            if(!zoneCount.getText().trim().equals("0")) {
+            if (!zoneCount.getText().trim().equals("0")) {
                 WebElement link = elements.get(i).findElement(By.cssSelector(":nth-child(5) a"));
                 link.click();
 
@@ -86,17 +87,48 @@ public class SortCountriesTest {
                 Collections.sort(zonesSorted);
 
                 for (int k = 0; k < zones.size(); k++) {
-                     Assert.assertEquals(zones.get(k), zonesSorted.get(k));
+                    Assert.assertEquals(zones.get(k), zonesSorted.get(k));
                 }
 
                 driver.get(BASE_URL + COUNTRIES_URL);
-                elements = driver.findElements(By.cssSelector("td#content form[name=countries_form] table.dataTable tbody tr.row"));
+                elements = driver.findElements(By.cssSelector(COUNTRIES_SELECTOR));
             }
 
         }
         logout();
     }
 
+    @Test()
+    public void verifyZones() {
+        login();
+        driver.get(BASE_URL + ZONES_URL);
+        List<WebElement> elements = driver.findElements(By.cssSelector(ZONES_SELECTOR));
+        for (int i = 0; i < elements.size(); i++) {
+            List<WebElement> geoCountries = driver.findElements(By.cssSelector(ZONES_SELECTOR));
+            geoCountries.get(i).click();
+
+
+            List<WebElement> geoZones = driver.findElements(By.cssSelector("td#content form table#table-zones.dataTable tbody tr td:nth-child(3) select option[selected]"));
+            ArrayList<String> zones = new ArrayList<>();
+
+            for (WebElement geoZone : geoZones) {
+                zones.add(geoZone.getText());
+            }
+
+            ArrayList<String> geoZonesSorted = new ArrayList<>(zones);
+            Collections.sort(geoZonesSorted);
+
+            for (int k = 0; k < zones.size(); k++) {
+                Assert.assertEquals(zones.get(k), geoZonesSorted.get(k));
+            }
+
+            driver.get(BASE_URL + ZONES_URL);
+            elements = driver.findElements(By.cssSelector(ZONES_SELECTOR));
+        }
+
+        logout();
+
+    }
 
 
 
